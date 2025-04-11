ad5045f73ab697f08a7511e1c394e5187259b040
Sai Deepak Varanasi <150058790+Deepu1004@users.noreply.github.com>
2025-04-11 22:32:00 +0530

    Time: 530 ms (76.37%) | Memory: 17.8 MB (54.99%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.py
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -1,23 +1,34 @@
 class Solution:
-    
+
+    def isSymmetric(self, num, n):
+        leftSum = 0
+        rightSum = 0
+
+        for i in range(n//2):
+            rightSum += num%10
+            num //= 10
+            
+        
+        for i in range(n//2):
+            leftSum += num%10
+            num //= 10
+
+        return leftSum == rightSum
+
+    def countTotalDigits(self, num):
+        count = 0
+        while(num > 0):
+            count += 1
+            num = num//10
+        return count
 
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        cnt = 0
-
-        for num in range(low, high+1):
-            if((num >= 10 and num<=99) and (num % 11 == 0) ):
-                cnt += 1
-            elif(num >= 1000 and num <= 9999):
-                left = 0
-                right = 0
-                for i in range(2):
-                    right += num%10
-                    num = num//10
-
-                for i in range(2):
-                    left += num%10
-                    num = num//10
-                if(left == right):
-                    cnt += 1
-    
-        return cnt
+        res = 0
+        for i in range(low, high+1):
+            count = self.countTotalDigits(i)
+            if(count % 2 != 0):
+                continue
+            elif(self.isSymmetric(i, count)): 
+                res += 1
+        
+        return res
