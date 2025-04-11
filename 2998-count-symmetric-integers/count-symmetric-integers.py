4aee84b48e8dfb9cdf4b2a4456c7e09097aff219
Sai Deepak Varanasi <150058790+Deepu1004@users.noreply.github.com>
2025-04-11 22:13:43 +0530

    Time: 340 ms (92.87%) | Memory: 17.8 MB (81.26%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.py
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -1,20 +1,17 @@
 class Solution:
-    def isPalindrome(self,s):
-        if(len(s) % 2 != 0):
-            return False
-        lsm,rsm = 0,0
-        for i in range(len(s)//2):
-            lsm += int(s[i])
-        for i in range(len(s)//2):
-            rsm += int(s[len(s)//2 + i])
-        if(lsm == rsm):
-            return True
-        else:
-            return False
+    
 
     def countSymmetricIntegers(self, low: int, high: int) -> int:
         cnt = 0
-        for i in range(low, high+1):
-            if(self.isPalindrome(str(i))):
-                cnt += 1
+
+        for num in range(low, high+1):
+            number = str(num)
+            if(len(number) == 2):
+                if(number[0] == number[1]):
+                    cnt += 1
+            elif(len(number) == 4):
+                left = int(number[0]) + int(number[1])
+                right = int(number[2]) + int(number[3])
+                if(left == right):
+                    cnt += 1
         return cnt
